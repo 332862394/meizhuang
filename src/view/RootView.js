ea00d5b96c510bb628f486bc21b31ff568d1e6d3
mov_zh <mov_zh@mov-zh.local>
2022-03-02 16:03:58 +0800

    完成播放器

--- src/view/RootView.js
+++ src/view/RootView.js
@@ -1,103 +1,173 @@
 import React,{useState,useRef} from 'react';
-import {View, Image, Dimensions,Text,StyleSheet,ActivityIndicator} from 'react-native';
+import {View, Image, Dimensions,Text,StyleSheet,ActivityIndicator,TouchableOpacity} from 'react-native';
 import Video from 'react-native-video';
+import Slider from '@react-native-community/slider';
+
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import VideoPlayer from '../components/VideoPlayer';
 const {width,height} = Dimensions.get('window');
+console.log("width:",width),
+console.log("height:",height)
+
 const bl = width / 1194;
 const url = 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
 
+
+
 const RootView = () => {
-  const [rate, setRate] = useState(1);
-  const [muted, setMuted] = useState(false);
+  const player = useRef(null);
 
-  const [resizeMode, setResizeMode] = useState('contain');
 
-  const [repeat, setRepeat] = useState(false);
   const [videoOk, setVideoOk] = useState(true);
   const [videoLoaded, setVideoLoaded] = useState(false);
   const [playing, setPlaying] = useState(false);
   const [paused, setPaused] = useState(false);
-  const [videoProgress, setVideoProgress] = useState(0.01);
   const [videoTotal, setVideoTotal] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
- 
+  const [sliderValue, setSliderValue] = useState(0);
   const onLoadStart = () => {
-    console.log("onLoadStart")
+    console.log('onLoadStart');
   };
   const onLoad = () => {
-    console.log("onLoad")
+    console.log('onLoad');
   };
-  const onProgress=(data)=>{
-    console.log("onProgress:",data)
+  const onProgress = data => {
     if (!videoLoaded) {
-      setVideoLoaded(t=>true)
-  }
-
-  // 视频中时长
-  let duration = data.seekableDuration;
-  let currentTime = data.currentTime;
-  // toFixed(2) get 小数点后两位
-  let percent = Number((currentTime / duration).toFixed(2));
-  setVideoTotal(t=>t=duration)
-
-  setCurrentTime(t=>t=Number(data.currentTime.toFixed(2)))
-  setVideoProgress(t=>t=percent)
-  
-
-  if (!videoLoaded) {
-      setVideoLoaded(t=>true)
-  }
-  // 视频暂停播放或播放结束，显示播放按钮
-  if (!playing) {
-      setPlaying(t=>true)
-  }
-  }
-  const onEnd=()=>{
-    console.log("onEnd")
-  }
+      setVideoLoaded(t => true);
+    }
+    // 视频中时长
+    let duration = data.seekableDuration;
+    setVideoTotal(t => (t = duration));
+    setCurrentTime(t => (t = Number(data.currentTime.toFixed(2))));
+    setSliderValue(t=>(t = Number(data.currentTime.toFixed(2))))
+    if (!videoLoaded) {
+      setVideoLoaded(t => true);
+    }
+    // 视频暂停播放或播放结束，显示播放按钮
+    if (!playing) {
+      setPlaying(t => true);
+    }
+  };
+  const onEnd = () => {
+    console.log('onEnd');
+    setPlaying(t=>t=false)
+  };
   const onError=()=>{
     console.log("onError")
+    setVideoOk(t=>t=false)
   }
+  const changePausedState=()=>{
+   
+    setPaused(t=>!t)
+   console.log("p")
+ 
+  }
+  const formatMediaTime=(time)=> {
+    let minute = Math.floor(time / 60);
+    let second = parseInt(time - minute * 60);
+    minute = minute >= 10 ? minute : "0" + minute;
+    second = second >= 10 ? second : "0" + second;
+    return minute + ":" + second;
+   
+}
+const customerSliderValue=(value)=>{
+  console.log("customerSliderValue")
+  player.current.seek(value)
+
+  
+}
+const nextVideo=()=>{
+  console.log("next")
+}
+const goodEvent=()=>{
+  console.log("good")
+}
+const shareEvent=()=>{
+  console.log("share")
+}
   return (
     <View style={styles.bigView}>
       <View style={styles.asideView}>
-        <Image
-        source={require('../res/logo.png')}
-        style={styles.logoView}
-      /></View>
+        {/* <Image source={require('../res/logo.png')} style={styles.logoView} /> */}
+        <Image source={require('../res/left_bkg.png')} style={styles.logoView2} />
+
+      </View>
       <View style={styles.maiView}>
         <View style={styles.headView}></View>
         <View style={styles.contentView}>
-         
-        <Video  source={{uri: url}}   // Can be a URL or a local file.
-       ref={(ref) => {
-         this.player = ref
-       }}        
-       resizeMode="cover"  
-        // 当视频开始加载时
-        onLoadStart={onLoadStart}
-        // 当视频在不断的加载时
-        onLoad={onLoad}
-        // 当视频播放时，每250ms调用一次，便于知悉当前播放位置(时间)
-        onProgress={onProgress}
-        // 当视频播放结束时调用
-        onEnd={onEnd}
-        // 当视频出错时调用
-        onError={onError}
-       style={styles.backgroundVideo} />  
-        {
-          !videoOk && <Text style={styles.failText}>视频出错了！很抱歉</Text>
-        }
-         {
-          !videoLoaded && <ActivityIndicator style={styles.loading} color="#ee735c"/>
-        }
-         <View style={styles.progressBox}>
-          <View style={[styles.progressBar, {width: (width-200*bl) * videoProgress}]}></View>
+          <Video
+            source={{uri: url}} // Can be a URL or a local file.
+            ref={player}
+            paused={paused}
+            resizeMode="cover"
+            // 当视频开始加载时
+            onLoadStart={onLoadStart}
+            // 当视频在不断的加载时
+            onLoad={onLoad}
+            // 当视频播放时，每250ms调用一次，便于知悉当前播放位置(时间)
+            onProgress={onProgress}
+            // 当视频播放结束时调用
+            onEnd={onEnd}
+            // 当视频出错时调用
+            onError={onError}
+            style={styles.backgroundVideo}
+          />
+          {!videoOk && <Text style={styles.failText}>视频出错了！很抱歉</Text>}
+          {!videoLoaded && (
+            <ActivityIndicator style={styles.loading} color="#ff4ebb" />
+          )}
+
         </View>
-      </View>
-      <View style={styles.controlView}><Text>11</Text></View>
+        <View style={styles.controlView}>
+            <View style={styles.sliderBox}>
+              <Text style={styles.timeView}>{formatMediaTime(currentTime)}</Text>
+              <Slider
+                style={styles.sliderView}
+                value={sliderValue}
+                maximumValue={videoTotal}
+                thumbTintColor="#ffffff" //开关夹点的颜色
+                minimumTrackTintColor="#ff4ebb"
+                maximumTrackTintColor="#e9e9e9"
+                step={0.01}
+                onValueChange={customerSliderValue}
+              />
+              <Text style={styles.timeView}>{formatMediaTime(videoTotal)}</Text>
+            </View>
+          {paused ? (
+            <TouchableOpacity
+              style={styles.btnView}
+              onPress={changePausedState}>
+              <Image
+                source={require('../res/play2.png')}
+                style={styles.playBtn}
+              />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              style={styles.btnView}
+              onPress={changePausedState}>
+              <Image
+                source={require('../res/play.png')}
+                style={styles.playBtn}
+              />
+            </TouchableOpacity>
+          )}
+          <TouchableOpacity style={styles.btnView2} onPress={nextVideo}>
+            <Image source={require('../res/next.png')} style={styles.playBtn} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnView3} onPress={goodEvent}>
+            <Image source={require('../res/good.png')} style={styles.playBtn} />
+            <Text>点赞</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnView4} onPress={shareEvent}>
+            <Image
+              source={require('../res/share.png')}
+              style={styles.playBtn}
+            />
+            <Text>分享</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -105,15 +175,16 @@ const RootView = () => {
 const styles=StyleSheet.create({
   bigView:{
     width: width,
-    height: height,
+    height: '100%',
     backgroundColor: 'yellow',
     flexDirection:'row',
-    // flex: 1
+    padding: 10,
   },
   asideView:{
     width:200*bl,
-    height: height,
+    height: "100%",
     backgroundColor:"white",
+    position: 'relative'
   },
  
   logoView:{
@@ -123,9 +194,16 @@ const styles=StyleSheet.create({
     marginTop:37*bl
 
   },
+  logoView2:{
+    position: 'absolute',
+    width: 200*bl,
+    height: 99*bl,
+bottom: 0,
+
+  },
   maiView:{
-    backgroundColor:"blue",
-    flex:1,
+    backgroundColor:"white",
+    flex:1,   
     flexDirection:'column',
     position: 'relative'
   },
@@ -166,67 +244,60 @@ const styles=StyleSheet.create({
     color: '#fff',
     backgroundColor: 'transparent'
   },
-  // 进度条样式
-  progressBox: {
-    width: "100%",
-    height: 2,
-    backgroundColor: '#ccc',
- 
-    position: 'absolute',
-    top: 0,
-    left: 0,
-  },
- 
-  progressBar: {
-    width: 1,
-    height: 2,
-    backgroundColor: '#ff6600'
-  },
-  // // 播放按钮样式
-  // playIcon: {
-  //   position: 'absolute',
-  //   top: 180*bl,
-  //   left:'50%' ,
-  //   marginLeft:30*bl,
-  //   width: 60*bl,
-  //   height: 60*bl,
-  //   paddingTop: 8*bl,
-  //   paddingLeft: 22*bl,
-  //   backgroundColor: 'transparent',
-  //   borderColor: '#fff',
-  //   borderWidth: 1,
-  //   borderRadius: 30,
-  //   color: '#ed7b66'
-  // },
-  // // 暂停
-  // pauseBtn: {
-  //   position: 'absolute',
-  //   left: 0,
-  //   top: 0,
-  //   width: width,
-  //   height: width * 0.56
-  // },
-  // 继续
-  // resumeIcon: {
-  //   position: 'absolute',
-  //   top: 80,
-  //   left: width / 2 - 30,
-  //   width: 60,
-  //   height: 60,
-  //   paddingTop: 8,
-  //   paddingLeft: 22,
-  //   backgroundColor: 'transparent',
-  //   borderColor: '#fff',
-  //   borderWidth: 1,
-  //   borderRadius: 30,
-  //   color: '#ed7b66'
-  // },
+  
   controlView:{
   height: 90*bl,
-  backgroundColor:'red',
   position: 'relative',
   left: 0,
   bottom: 0,
-  }
+  width: '100%',
+  },
+ 
+btnView:{
+  position: 'absolute',
+  left: 24*bl,
+  top: 50*bl
+},
+btnView2:{
+  position: 'absolute',
+  left: 74*bl,
+  top: 50*bl
+},
+btnView3:{
+  position: 'absolute',
+  justifyContent:'center',
+  alignItems:'center',
+  right: 102*bl,
+  top: 21*bl
+},
+btnView4:{
+  position: 'absolute',
+  justifyContent:'center',
+  alignItems:'center',
+  right: 32*bl,
+  top: 21*bl
+},
+playBtn:{
+  width: 24*bl,
+  height: 24*bl,
+  margin: 7*bl
+},
+timeView:{
+  fontSize:16*bl,
+  color: '#666666'
+},
+sliderBox:{
+    flexDirection:'row',
+    alignItems:'center',
+    position: 'absolute',
+    top: 5*bl,
+    left: 24*bl,
+    right: 152*bl
+},
+sliderView:{
+  flex: 1,
+height: 40*bl
+}
+
 })
 export default RootView;
